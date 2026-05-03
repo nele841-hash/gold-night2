1731288731eb686b90ed6a99d03bba1edbe5411c
nele841-hash <avdija841@gmail.com>
2026-05-03 19:47:13 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1584,12 +1584,12 @@ async def avatar(ctx, member: discord.Member = None):
 
 #---------------šamar--------------
 @bot.command()
-async def ošamari(ctx, member: discord.Member):
+async def osamari(ctx, member: discord.Member):
+
     gifs = [
-        "https://media.tenor.com/wF0Xz7k5vQ0AAAAC/slap-anime.gif",
-        "https://media.tenor.com/0G2bX5x0kqMAAAAC/slap.gif",
-        "https://media.tenor.com/8gqQ9x7dZ7sAAAAC/slap-face.gif",
-        "https://media.tenor.com/3Qw8d5m3s0MAAAAC/anime-slap.gif"
+        "https://media.tenor.com/images/5c1b6f7c2d8a3d7f0f6b1f0a3c8d0a3f/tenor.gif",
+        "https://media.tenor.com/images/2d6b7a9c1f3e4b5a6c7d8e9f0a1b2c3d/tenor.gif",
+        "https://media.tenor.com/images/9f8e7d6c5b4a3210f0e1d2c3b4a5f6e7/tenor.gif"
     ]
 
     gif = random.choice(gifs)
